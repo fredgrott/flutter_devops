af532b87321c322ce4fde56ff194152978d2d3bc
Fred Grott <fred.grott@gmail.com>
2021-08-18 09:37:00 -0500

    first commit
    first commit

---
+++ basic_buildvariants/android/app/src/main/kotlin/io/github/fredgrott/basic_buildvariants/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fredgrott.basic_buildvariants
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ basic_testing/android/app/src/main/kotlin/io/github/fredgrott/basic_testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.fredgrott.basic_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
